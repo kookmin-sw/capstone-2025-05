b4ffb4bfdc3e63dd0af948e20e4483e0f41283c7
Hoontheduck <lovelyoverflow@gmail.com>
2025-01-31 00:51:02 +0900

    Update build-windows.yml to download Projucer instead of building it

--- Source/UI/Styles/MapleColours.h
+++ Source/UI/Styles/MapleColours.h
@@ -3,8 +3,49 @@
 
 namespace MapleColours
 {
-    const juce::Colour buttonNormal = juce::Colour(0xFF007AFF);
-    const juce::Colour buttonHover = juce::Colour(0xFF3395FF);
-    const juce::Colour buttonPressed = juce::Colour(0xFF0056B3);
-    const juce::Colour buttonText = juce::Colour(0xFFFFFFFF);
+    struct Theme
+    {
+        juce::Colour background;
+        juce::Colour foreground;
+        juce::Colour buttonNormal;
+        juce::Colour buttonHover;
+        juce::Colour buttonPressed;
+        juce::Colour buttonText;
+        juce::Colour text;
+        juce::Colour border;
+    };
+
+    namespace Dark
+    {
+        const Theme theme{
+            juce::Colour(0xFF121212), // background
+            juce::Colour(0xFF1E1E1E), // foreground
+            juce::Colour(0xFF007AFF), // buttonNormal
+            juce::Colour(0xFF3395FF), // buttonHover
+            juce::Colour(0xFF0056B3), // buttonPressed
+            juce::Colour(0xFFFFFFFF), // buttonText
+            juce::Colour(0xFFE0E0E0), // text
+            juce::Colour(0xFF2D2D2D)  // border
+        };
+    }
+
+    namespace Light
+    {
+        const Theme theme{
+            juce::Colour(0xFFFAFAFA), // background
+            juce::Colour(0xFFFFFFFF), // foreground
+            juce::Colour(0xFF007AFF), // buttonNormal
+            juce::Colour(0xFF3395FF), // buttonHover
+            juce::Colour(0xFF0056B3), // buttonPressed
+            juce::Colour(0xFFFFFFFF), // buttonText
+            juce::Colour(0xFF000000), // text
+            juce::Colour(0xFFE0E0E0)  // border
+        };
+    }
+
+    // 현재 활성화된 테마를 저장할 전역 변수
+    extern Theme currentTheme;
+
+    // 테마 전환 함수
+    void setTheme(bool isDark);
 }
