f3624a1d3700ac6df6b122a6e759d6c1edd8aaa9
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-03-01 00:15:24 +0900

    design: content 가운데 정렬

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Box({ width, height, content }) {
-    return (
-      <div
-        className="bg-[#A57865] text-white rounded-[10px] cursor-pointer"
-        style={{ width, height }}
-      >
-        {content}
-      </div>
-    );
+export default function Box({ width, height, children }) {
+  return (
+    <div
+      className="bg-[#A57865] text-white rounded-[10px] cursor-pointer flex justify-center items-center"
+      style={{ width, height }}
+    >
+      {children}
+    </div>
+  );
 }
