2532bfe95e86ec6ec392950d668186e73d952eb5
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-02-28 23:50:30 +0900

    app.js에서 헤더 푸터 지움

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import TestPage from './Pages/TestPage/testPage';
-import MapleHeader from '../src/Components/MapleHeader';
-import MapleFooter from './Components/MapleFooter';
 
 function App() {
   return (
