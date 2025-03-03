05a3e04d5e2033f66f7d10e50b50130497f2607e
dgKim1 <kimdg3556@naver.com>
2025-03-03 17:51:59 +0900

    feat: 글쓰기 페이지 1차 디자인 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import TestPage from './Pages/TestPage/testPage';
+import WritePage from './Pages/WritePage/writePage';
+import MapleHeader from './Components/MapleHeader';
+import MapleFooter from './Components/MapleFooter';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <MapleHeader />
         <Routes>
           <Route path="/test" element={<TestPage />} />
+          <Route path="/write" element={<WritePage />} />
         </Routes>
+        <MapleFooter
+          github={'kimdg@github.com'}
+          email={'kimdk3556@gmail.com'}
+        />
       </BrowserRouter>
     </div>
   );
