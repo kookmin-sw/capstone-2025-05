052a75262d87e26bc202d4ba6422ad00d798c7a9
dgKim1 <kimdg3556@naver.com>
2025-03-07 16:38:24 +0900

    임시저장

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import TestPage from './Pages/TestPage/testPage';
 import WritePage from './Pages/WritePage/writePage';
+import NoticeBoard from './Pages/NoticeBoard/noticeBoard';
 import MapleHeader from './Components/MapleHeader';
 import MapleFooter from './Components/MapleFooter';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/test" element={<TestPage />} />
           <Route path="/write" element={<WritePage />} />
+          <Route path="/notice" element={<NoticeBoard />} />
         </Routes>
         <MapleFooter
           github={'kimdg@github.com'}
