9ba70204e1c6ea7b6ae7e6a4b3b6122adc162263
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-03-10 00:10:39 +0900

    feat: 실력 및 장르 선택 추가

--- src/Pages/Login/profile.js
+++ src/Pages/Login/profile.js
@@ -2,21 +2,36 @@ import React, { useState } from 'react';
 import Header from '../../Components/MapleHeader';
 import Logo from '../../Assets/logo.svg';
 import Input from '../../Components/Input/input.js';
+import Dropdown from '../../Components/Dropdown/dropdown.js';
 import Button from '../../Components/Button/Button.js';
 import Footer from '../../Components/MapleFooter';
 
 export default function Profile() {
   const [nickname, setNickname] = useState('');
+  const [skillLevel, setSkillLevel] = useState('');
+  const [genre, setGenre] = useState('');
 
   const handleNickname = (e) => {
     setNickname(e.target.value);
   };
 
+  const handleSkillChange = (e) => {
+    setSkillLevel(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
   const invalidNickname = nickname.length === 0 || nickname.length > 10;
+  const invalidSkill = skillLevel === '';
+  const invalidGenre = genre === '';
 
   const handleComplete = () => {
-    if (!invalidNickname) {
+    if (!invalidNickname && !invalidSkill && !invalidGenre) {
       console.log(nickname);
+      console.log(skillLevel);
+      console.log(genre);
       console.log('complete!');
     }
   };
@@ -25,15 +40,15 @@ export default function Profile() {
     <div className="flex flex-col h-screen">
       <Header />
       <div className="flex flex-col items-center flex-grow relative">
-        <div className="absolute top-[20%]">
+        <div className="absolute top-[12%]">
           <img src={Logo} alt="logo" className="w-60 h-auto" />
         </div>
 
-        <span className="absolute top-[40%] text-[20px] font-bold">
-          설정할 닉네임을 입력해 주세요
+        <span className="absolute top-[32%] text-[20px] font-bold">
+          설정할 닉네임을 입력하고 실력과 선호 장르를 선택해 주세요
         </span>
 
-        <div className="absolute top-[52%]">
+        <div className="absolute top-[44%]">
           <Input
             width="400px"
             height="52px"
@@ -44,15 +59,41 @@ export default function Profile() {
           />
         </div>
 
-        <div className="absolute top-[76%] text-center">
+        <div className="absolute top-[56%]">
+          <Dropdown
+            name="skillLevel"
+            value={skillLevel}
+            onChange={handleSkillChange}
+            width="400px"
+            height="52px"
+          />
+        </div>
+
+        <div className="absolute top-[68%]">
+          <Dropdown
+            name="genre"
+            value={genre}
+            onChange={handleGenreChange}
+            width="400px"
+            height="52px"
+          />
+        </div>
+
+        <div className="absolute top-[88%] text-center">
           <Button
             width="100px"
             height="40px"
             onClick={handleComplete}
-            disabled={invalidNickname}
+            disabled={invalidNickname || invalidSkill || invalidGenre}
             style={{
-              backgroundColor: invalidNickname ? '#AFAFAF' : '',
-              cursor: invalidNickname ? 'default' : 'pointer',
+              backgroundColor:
+                invalidNickname || invalidSkill || invalidGenre
+                  ? '#AFAFAF'
+                  : '',
+              cursor:
+                invalidNickname || invalidSkill || invalidGenre
+                  ? 'default'
+                  : 'pointer',
             }}
           >
             완료
