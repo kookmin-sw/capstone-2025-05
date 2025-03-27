55b79c2f8ac3ca7bab41294d9cdeb72bd9f1a6cc
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-03-27 19:44:52 +0900

    feat: 자체 로그인 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import TestPage from './Pages/TestPage/testPage';
 import Login from './Pages/Login/login';
+import Signup from './Pages/Login/signup';
 import Profile from './Pages/Login/profile';
 import Main from './Pages/Main/main';
 import WritePage from './Pages/WritePage/writePage';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/test" element={<TestPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/main" element={<Main />} />
           <Route path="/write" element={<WritePage />} />
