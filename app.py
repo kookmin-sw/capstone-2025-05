0e0991c4b153feb7d66ea9fd7464ee6da5a3f19b
syngrxm <smcubez48@naver.com>
2025-04-11 16:16:20 +0900

    [FIX] add new router

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from routers.myPage_router import router as account_router
 # 게시판 import
-# 마이페이지 import
+from routers.myPage_router import router as myPage_router
 from routers.spotify_router import router as spotify_router
 from routers.recent_prepare_router import router as recent_prepared_router
 from routers.ranking_router import router as ranking_router
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.include_router(account_router)
 #app.include_router(post_router)
-#app.include_router(myPage_router)
+app.include_router(myPage_router)
 app.include_router(spotify_router)
 app.include_router(ranking_router)
 app.include_router(recent_prepared_router)
