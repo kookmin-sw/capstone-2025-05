e894bbc167ccf26e5e8efb9afd7a384af21b0cb5
dgKim1 <kimdg3556@naver.com>
2025-05-06 12:38:13 +0900

    fix: 구글로그인 기능 수정

--- src/Pages/Google/OauthRedirectHandler.js
+++ src/Pages/Google/OauthRedirectHandler.js
@@ -1,17 +1,32 @@
 import { useEffect } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../Context/AuthContext';
 
 export default function OauthRedirectHandler() {
+  const location = useLocation();
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
   const { login } = useAuth();
-  console.log('실행됨');
+
   useEffect(() => {
-    const uid = searchParams.get('uid');
+    const hash = window.location.hash; // #/oauth-success?uid=xxx
+    console.log('hash', hash);
+    const query = hash.split('?')[1];
+    const params = new URLSearchParams(query);
+    const uid = params.get('uid');
+
+    if (hash.startsWith('#/oauth-success')) {
+      if (uid) {
+        console.log('✅ App.js에서 OAuth uid 감지됨:', uid);
+        login(uid); // context에 저장
+        sessionStorage.setItem('uid', uid); // 혹시 몰라 수동 저장
+        window.location.hash = '#/main'; // 강제 이동
+      }
+    }
+
+    console.log('OAuth 진입:', uid);
     if (uid) {
-      login(uid); // Context 로그인 처리
-      navigate('/main'); // 메인 페이지로 이동
+      login(uid);
+      navigate('/main');
     }
   }, []);
 
