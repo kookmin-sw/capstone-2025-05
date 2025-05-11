8f1e9dc24e04c3d641e9c8e2a15f969ebd96a439
dgKim1 <kimdg3556@naver.com>
2025-05-12 01:53:58 +0900

    fix: 오타 수정

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Profile({ profile_url }) {
   return (
     <div className="h-[50px] w-[50px] rounded-full overflow-hidden shadow-md mr-2">
-      <img src={profile_url} className="object-cove" />
+      <img src={profile_url} className="object-cover" />
     </div>
   );
 }
